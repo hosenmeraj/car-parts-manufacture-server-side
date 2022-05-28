c8db832326a842ce3db2358b1830643fab02edb8
Merajhosen <merajakhon7@gmail.com>
2022-05-28 08:29:24 +0600

    getting all order with each email id

--- index.js
+++ index.js
@@ -19,25 +19,27 @@ async function run() {
         const partsCollection = client.db("car_parts").collection("services");
         const orderCollection = client.db("car_parts").collection("orders");
 
+        //get load all data
         app.get('/services', async (req, res) => {
             const query = {}
             const cursor = partsCollection.find(query)
             const services = await cursor.toArray()
             res.send(services)
         })
-
+        //inseart one tools
         app.get('/service/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
             const result = await partsCollection.findOne(query)
             res.send(result)
         })
+        //add a new tools
         app.post('/order', async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result);
         })
-
+        //updta a quantity
         app.put("/service/:id", async (req, res) => {
             const id = req.params.id
             const updateQuantity = req.body
@@ -49,6 +51,14 @@ async function run() {
             const result = await partsCollection.updateOne(filter, updateDoc, options)
             res.send(result)
 
+
+        })
+        //my order
+        app.get('/order', async (req, res) => {
+            const email = req.query.email
+            const query = { email: email }
+            const orders = await orderCollection.find(query).toArray()
+            res.send(orders)
         })
 
 
